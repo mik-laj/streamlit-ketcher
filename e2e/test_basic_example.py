4c65ece3971667dce03cc876d0588db283dcafc5
Kamil Breguła <mik-laj@users.noreply.github.com>
2023-07-29 09:53:58 +0200

    Refactor E2E setup (#9)

--- e2e/test_basic_example.py
+++ e2e/test_basic_example.py
@@ -1,66 +1,29 @@
-import socket
-import subprocess
-import sys
-from contextlib import closing
 from pathlib import Path
 
 import pytest
 
 from playwright.sync_api import Page, expect
-import requests
 
-from requests.adapters import HTTPAdapter, Retry
+from e2e.e2e_utils import StreamlitRunner
 
 ROOT_DIRECTORY = Path(__file__).parent.parent.absolute()
+BASIC_EXAMPLE_FILE = ROOT_DIRECTORY / "e2e" / "apps" / "basic_example.py"
 
 
-def find_free_port():
-    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
-        s.bind(("", 0))
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        return s.getsockname()[1]
-
-
-def create_http_session():
-    s = requests.Session()
-
-    retries = Retry(total=5, backoff_factor=0.1)
-    s.mount("http://", HTTPAdapter(max_retries=retries))
-
-    return s
-
-
-@pytest.fixture(autouse=True, scope="session")
+@pytest.fixture(autouse=True, scope="module")
 def streamlit_app():
-    streamlit_app_path = ROOT_DIRECTORY / "e2e" / "apps" / "basic_example.py"
-    server_port = find_free_port()
-
-    with subprocess.Popen(
-        [
-            sys.executable,
-            "-m",
-            "streamlit",
-            "run",
-            str(streamlit_app_path),
-            f"--server.port={server_port}",
-            "--server.headless=true",
-        ]
-    ) as process1:
-        server_url = f"http://localhost:{server_port}"
-        try:
-            # Wait for webserver
-            with create_http_session() as http_session:
-                assert http_session.get(server_url + "/_stcore/health").text == "ok"
-            yield server_url
-        finally:
-            process1.kill()
+    with StreamlitRunner(BASIC_EXAMPLE_FILE) as runner:
+        yield runner
 
 
-def test_should_return_user_input(page: Page, streamlit_app: str, assert_snapshot):
-    page.goto(streamlit_app)
-
+@pytest.fixture(autouse=True, scope="function")
+def go_to_app(page: Page, streamlit_app: StreamlitRunner):
+    page.goto(streamlit_app.server_url)
     # Wait for app to load
     page.get_by_role("img", name="Running...").is_hidden()
+
+
+def test_should_return_user_input(page: Page, assert_snapshot):
     frame_0 = page.frame_locator(
         'iframe[title="streamlit_ketcher\\.streamlit_ketcher"]'
     )
@@ -83,11 +46,7 @@ def test_should_return_user_input(page: Page, streamlit_app: str, assert_snapsho
     expect(page.get_by_text("Smile code")).to_have_text("Smile code: C1C=CC=CC=1")
 
 
-def test_should_render_user_input(page: Page, streamlit_app: str, assert_snapshot):
-    page.goto(streamlit_app)
-
-    # Wait for app to load
-    page.get_by_role("img", name="Running...").is_hidden()
+def test_should_render_user_input(page: Page, assert_snapshot):
     page.get_by_role("textbox", name="Molecule").click()
     page.get_by_role("textbox", name="Molecule").fill("CCO")
     page.get_by_role("textbox", name="Molecule").press("Enter")
